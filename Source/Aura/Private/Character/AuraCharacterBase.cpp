66aac65fe448824b43b98492de0f1061da57d081
Lev Gura <frontend.leogura@gmail.com>
2025-03-28 18:33:45 +0400

    add enemy and character animation

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,18 @@ AAuraCharacterBase::AAuraCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+
+	// создал под объект с типом меш компонента и обозвал веапон
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+
+	// установил атачмент в с названием соккета, получив сначала меш родителя, получается того чара которого я ввызвал
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	// тут просто коллизию включил с вариантом noCollision
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	Belt = CreateDefaultSubobject<USkeletalMeshComponent>("Belt");
+	Belt->SetupAttachment(GetMesh(),FName("BeltSocket"));
+	Belt->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
